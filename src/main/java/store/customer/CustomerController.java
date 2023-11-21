56e46df9b2e105eae0bbb92efb219a629889de1c
Humberto Sandmann <hsandmann@ieee.org>
2023-11-21 00:06:26 -0300

    login

--- src/main/java/store/customer/CustomerController.java
+++ src/main/java/store/customer/CustomerController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
