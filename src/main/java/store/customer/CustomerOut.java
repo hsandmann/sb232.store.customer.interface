4b6ed4bc2815d9067ea59636c5c91d0b8916dcf3
Humberto Sandmann <hsandmann@ieee.org>
2023-12-08 12:10:12 -0300

    date

--- src/main/java/store/customer/CustomerOut.java
+++ src/main/java/store/customer/CustomerOut.java
@@ -1,11 +1,9 @@
 package store.customer;
 
-import java.util.Date;
-
 public record CustomerOut(
     String id,
     String name,
-    Date birthdate,
+    String birthdate,
     String cpf,
     String email
 ) {
